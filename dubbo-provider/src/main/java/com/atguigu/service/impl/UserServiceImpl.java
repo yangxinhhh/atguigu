bd2544abfbaafde33c1a331f3e149ffb86025272
yangxin <1052422959@qq.com>
2023-02-14 19:57:23 +0800

    第二次提交

---
+++ dubbo-provider/src/main/java/com/atguigu/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.atguigu.service.impl;
+
+import com.alibaba.druid.support.json.JSONUtils;
+
+/**
+ * @author: 杨鑫
+ * @date: 2023/2/14
+ * @desc:
+ */
+public class UserServiceImpl {
+    public void test(){
+        System.out.println("hello world!");
+    }
+}
